c34f50ab28f6b889de318055e3cede33c73055aa
abhishek pakrashi <ab.posts@gmail.com>
2021-11-24 19:33:30 +0530

    creates a custom list based on input

---
+++ demos/floc_sample/createSiteList.py
@@ -0,0 +1,12 @@
+import json
+siteList = []
+for i in range(1,9):
+    siteList.append(input(f'Enter site {i}: '))
+print(siteList)
+
+
+#print(siteList)
+
+
+with open('host_list.json', 'w', encoding='utf-8') as f:
+    json.dump(siteList, f, ensure_ascii=False, indent=4)
